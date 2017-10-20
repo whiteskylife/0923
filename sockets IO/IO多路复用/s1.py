16e8e389f62f4ee576fd837a6ccf41abf60aa1f8
w-PC\w <981577040@qq.com>
2017-10-20 16:50:14 +0800

    Merge branch 'master' of https://github.com/whiteskylife/incompany
    Conflicts:
    	.idea/.name
    	.idea/misc.xml
    	.idea/modules.xml
    	.idea/vcs.xml
    	.idea/workspace.xml
    	day2/三级菜单.py

--- sockets IO/IO多路复用/s1.py
+++ sockets IO/IO多路复用/s1.py
@@ -23,7 +23,12 @@ inputs = [sk1, sk2, sk3, ]
 
 while True:
     # [sk1, sk2, ], select内部自动监听sk1,sk2,sk3等对象，一旦某个句柄发生变化，写入r_list中
-    r_list, w_list, e_list = select.select(inputs, [], [], 1)  # 最后一个参数1表示最多延迟1秒用于select检测，如果超时了，此次循环跳过
+    r_list, w_list, e_list = select.select(inputs, [], [], 1)
+    # r_list: 第一个位置参数的传参，发生变化的对象放到r_list中
+    # w_list: 第二个位置参数列表中只要有值，就放入w_list中，没有值，则w_list为空
+    # e_list: 第三个位置参数列表中出错的socket放入e_list, 并从r_list中去掉出错的对象
+    # 最后一个参数1表示最多延迟1秒用于select检测，如果超时了，此次循环跳过
+
     for sk in r_list:
         conn, ip = sk.accept()
         conn.sendall(bytes('hello', encoding='utf-8'))
