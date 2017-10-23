db9d966b0354d7be9811f9c94fc1275b5937f2b1
w-PC\w <981577040@qq.com>
2017-10-23 11:46:43 +0800

    Merge branch 'master' of https://github.com/whiteskylife/incompany
    Conflicts:
    	.idea/.name
    	.idea/misc.xml
    	.idea/modules.xml
    	.idea/vcs.xml
    	.idea/workspace.xml
    	day2/三级菜单.py

--- sockets IO/IO多路复用/s1.py
+++ sockets IO/IO多路复用/s1.py
@@ -15,8 +15,17 @@ sk1.listen()
 inputs = [sk1, ]
 
 while True:
-
-
+    r_list, w_list, e_list = select.select(inputs, [], inputs, 1)
+    print('listening obj %d' % len(inputs))
+    print(r_list)
+    for sk in r_list:
+        if sk == sk1:
+            conn, ip = sk.accept()
+            inputs.append(conn)
+        else:
+            data_bytes = sk.recv(1024)
+            data_str = str(data_bytes, encoding='utf-8')
+            sk.sendall(bytes(data_str + 'ok', encoding='utf-8'))
 
 
 
