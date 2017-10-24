fcc951ad45dceecee96fe5f2d657f2b6c166139b
w-PC\w <981577040@qq.com>
2017-10-24 16:01:32 +0800

    Merge branch 'master' of https://github.com/whiteskylife/home
    Conflicts:
    	sockets IO/IO多路复用/s1.py

--- sockets IO/IO多路复用/s1.py
+++ sockets IO/IO多路复用/s1.py
@@ -12,27 +12,33 @@ sk1.bind(ip1)
 sk1.listen()
 
 inputs = [sk1, ]
-
+outputs = []
 while True:
-    r_list, w_list, e_list = select.select(inputs, [], inputs, 1)  # select内部自动监听sk1,sk2,sk3等对象，一旦某个句柄发生变化，写入r_list中)
+    r_list, w_list, e_list = select.select(inputs, outputs, inputs, 1)  # select内部自动监听sk1,sk2,sk3等对象，一旦某个句柄发生变化，写入r_list中)
     # 每当有一个用户来连接时，sk1都会发生变化，select将会监听到sk1的变化, 放入r_list中，而用户的conn对象只有在有数据传输时才会发生变化。
     print('listening obj %d' % len(inputs))
     print(r_list)
-    for sk_or_conn in r_list:
+    for sk_or_conn in r_list:           # r_list 用于读取信息
         # 每有一个新的连接对象时，sk发生变化，放入r_list中
         if sk_or_conn == sk1:
             conn, ip = sk_or_conn.accept()
             inputs.append(conn)     # 把新的连接对象放入r_list中
         else:
-            # 有用户发消息了
+            # 有老用户发消息了
             try:
                 data_bytes = sk_or_conn.recv(1024)
-                data_str = str(data_bytes, encoding='utf-8')
-                sk_or_conn.sendall(bytes(data_str + 'ok', encoding='utf-8'))
             except Exception as ex:
+                # 如果用户终止连接
+                print(ex)
                 inputs.remove(sk_or_conn)
+            else:
+                # # 用户正常发送消息
+                # data_str = str(data_bytes, encoding='utf-8')
+                # sk_or_conn.sendall(bytes(data_str + 'ok', encoding='utf-8'))
+                outputs.append(sk_or_conn)
 
-
-
+    for conn in w_list:                 # w_list 用于回复信息
+        conn.sendall(bytes('hello', encoding='utf-8'))
+        outputs.remove(conn)
 
 
