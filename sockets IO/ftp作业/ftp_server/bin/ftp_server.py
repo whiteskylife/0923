cb5218fe610c0b4a758b76f9b858eeec0cee3548
Administrator <981577040@qq.com>
2017-10-30 00:06:02 +0800

    commit

--- sockets IO/ftp作业/ftp_server/bin/ftp_server.py
+++ sockets IO/ftp作业/ftp_server/bin/ftp_server.py
@@ -4,11 +4,11 @@
 import os
 import sys
 
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
 
 from core import main
 
 if __name__ == '__main__':
-    main.ArgvHandler(sys.argv)
+    main.ArgvHandler()
--- sockets IO/ftp作业/ftp_server/core/main.py
+++ sockets IO/ftp作业/ftp_server/core/main.py
@@ -1,2 +1,14 @@
 #!/usr/bin/env python
 # -*- coding utf-8 -*-
+
+import optparse
+
+
+class ArgvHandler(object):
+    def __init__(self):
+        parser = optparse.OptionParser()
+        parser.add_option('-s', '--host', dest='host', help='server binding host address')
+        parser.add_option('-p', '--port', dest='port', help='server binding port')
+        (options, args) = parser.parse_args()   # 解析输入的参数是否符合规则，把结果返回（options：add_options中输入的参数，args，不在add_option中的参数）
+        print('parser', options, args)
+        print(options.host, options.port)
