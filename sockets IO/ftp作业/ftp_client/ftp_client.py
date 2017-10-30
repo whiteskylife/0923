ae3c71220495e0d43fa76b2dc20642f20e62f85c
w-PC\w <981577040@qq.com>
2017-10-30 14:56:02 +0800

    commit

--- sockets IO/ftp作业/ftp_client/ftp_client.py
+++ sockets IO/ftp作业/ftp_client/ftp_client.py
@@ -1,2 +1,18 @@
 #!/usr/bin/env python
 # -*- coding utf-8 -*-
+
+import os
+import json
+import optparse
+
+
+class FTPClient(object):
+    def __init__(self):
+        parser = optparse.OptionParser()
+        parser.add_option('-s', '--server', dest='server', help='ftp server ip addr')
+        parser.add_option('-p', '--port', dest='port', help='ftp server port')
+        parser.add_option('-u', '--username', dest='username', help='ftp server username')
+        parser.add_option('-P', '--password', dest='password', help='password')
+
+    def interactive(self):
+        pass
