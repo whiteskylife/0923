f49f0a231c90ddfd3147c52ea6e5480bf1d60210
w-PC\w <981577040@qq.com>
2017-10-30 15:38:05 +0800

    commit

--- sockets IO/ftp作业/ftp_client/ftp_client.py
+++ sockets IO/ftp作业/ftp_client/ftp_client.py
@@ -10,9 +10,29 @@ class FTPClient(object):
     def __init__(self):
         parser = optparse.OptionParser()
         parser.add_option('-s', '--server', dest='server', help='ftp server ip addr')
-        parser.add_option('-p', '--port', dest='port', help='ftp server port')
-        parser.add_option('-u', '--username', dest='username', help='ftp server username')
-        parser.add_option('-P', '--password', dest='password', help='password')
+        parser.add_option('-P', '--port', type='int', dest='port', help='ftp server port')
+        parser.add_option('-u', '--username', dest='username', help='username')
+        parser.add_option('-p', '--password', dest='password', help='password')
+        options, args = parser.parse_args()
+        self.verify_args(options, args)
+
+    def verify_args(self, options, args):
+        """
+        校验参数合法性
+        :param options:
+        :param args:
+        :return:
+        """
+        if options.server and options.port:
+            print(options)
+            if options.port >0 and options.port <65535:
+                pass
+            else:
+                print('Err: host port must in 0-65535')
 
     def interactive(self):
         pass
+
+if __name__ == '__main__':
+    ftp = FTPClient()
+    ftp.interactive()
---
+++ sockets IO/ftp作业/test/get_dict.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+dict = {'xiaoming': '123', 'whisky': '666'}
+
+a = dict.get('whisky')
+print(a)
