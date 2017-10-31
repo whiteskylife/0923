46c80f72a1d076a0457b7e15ce8f8105f1a37800
w-PC\w <981577040@qq.com>
2017-10-31 11:01:49 +0800

    commit

--- sockets IO/ftp_作业/FTPClient/FTPClient.py
+++ sockets IO/ftp_作业/FTPClient/FTPClient.py
@@ -1,3 +1,8 @@
 #!/usr/bin/env python
 # -*- coding utf-8 -*-
 
+import sys
+from modules import client
+
+if __name__ == '__main__':
+    client.Client(sys.argv)
--- sockets IO/ftp_作业/FTPServer/FTPServer.py
+++ sockets IO/ftp_作业/FTPServer/FTPServer.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python
 # -*- coding utf-8 -*-
+
+import os
+import sys
+
+from modules import main
+
+if __name__ == '__main__':
+    main.ArgvHandler(sys.argv)
+
