67341808a698e48e24d143692840e914455bfc15
w-PC\w <981577040@qq.com>
2017-11-06 16:50:03 +0800

    commit

--- sockets IO/ftp_作业/FTPServer/conf/settings.py
+++ sockets IO/ftp_作业/FTPServer/conf/settings.py
@@ -9,11 +9,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 USER_HOME = '%s\\var\\users' % BASE_DIR
 
 USER_ACCOUNT = {
-    'alex':{
-        'password': 'e10adc3949ba59abbe56e057f20f883e'
+    'alex': {
+        'password': 'e10adc3949ba59abbe56e057f20f883e',
+        'storage_limit': 2097152
     },
-    'whisky':{
-      'password': '202cb962ac59075b964b07152d234b70'
+    'whisky': {
+      'password': '202cb962ac59075b964b07152d234b70',
+      'storage_limit': 2097152
     },
 
 }
--- sockets IO/ftp_作业/FTPServer/modules/threading_socket_server.py
+++ sockets IO/ftp_作业/FTPServer/modules/threading_socket_server.py
@@ -43,13 +43,33 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
             self.instruction_distributor(data)
 
     def instruction_distributor(self, instructions):
+        """
+        指令分发函数
+        :param instructions: 客户端发送的指令信息
+        :return:
+        """
         print('in1', instructions)
         instructions = instructions.split('|')
         print('in2', instructions)
 
-        function_str = instructions[0]
-        if hasattr(self, function_str):
+        function_str = instructions[0]      # 第一个参数user_auth，第二个参数：用户名、密码
+        if hasattr(self, function_str):     # 有没有用户验证方法
             func = getattr(self, function_str)
-            func(instructions[1])
+            func(instructions[1])           # 此处仍是json格式
         else:
             print('Invalid instruction')
+
+    def user_auth(self, data):
+        """
+        用户认证
+        :param data: 用户、密码等信息（instructions[1]）
+        :return:
+        """
+        auth_info = json.loads(data)        # 格式为字典
+        if auth_info['username'] in settings.USER_ACCOUNT:   # 检测客户端发来的用户名是否在配置文件中
+            if settings.USER_ACCOUNT[auth_info['uaername']]['password '] == auth_info['password']:
+                #本地文件存储的密码和客户端发来的密码比对
+                self.login_user =
+
+    def login_user(self):
+        pass
