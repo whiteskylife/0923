44a0b3dcb5a78786b434a0075f280a8d35452daf
w-PC\w <981577040@qq.com>
2017-11-22 16:20:21 +0800

    commit

--- day9 线程 进程 协程/test.py
+++ day9 线程 进程 协程/test.py
@@ -7,22 +7,33 @@ from multiprocessing import Pool
 import time
 
 
-def f1(a):
-    time.sleep(1)
-    print(a)
-    return 1000
+# def fab(max):
+#     n, a, b = 0, 0, 1
+#     while n < max:
+#         yield
+#         #print(b)
+#         a, b = b, a + b
+#         n = n + 1
+#
+#
+# #fab(5)
+#
+# for i in fab(5):
+#     print(i)
+#
+#
 
 
-def f2(arg):
-    print(arg)
+def xrange():
+    print('start')
+    start = 0
+    while True:
+        yield
+        start += 1
 
+obj = xrange()
+r = obj.__next__()
+r = obj.__next__()
+r = obj.__next__()
+print(r)
 
-if __name__ == '__main__':
-    pool = Pool(5)
-    for i in range(5):
-        #pool.apply(func=f1, args=(i,))       # apply申请进程池中的进程的时候是一个一个去申请并执行，执行后才申请下一个, 相当于加了一个join方法
-        pool.apply_async(func=f1, args=(i,), callback=f2)   # 异步申请进程，主进程没有等待子进程执行完再退出
-        print('111111111111111111')
-    pool.close()  # for循环中的任务执行完后，关闭进程池，终止主进程
-    # pool.terminate()  # 立即终止所有for循环中的任务。
-    pool.join()         # 进程池的join方法（等所有的子进程执行完，主进程终止），必须先执行close方法。（查看源码可知，如果没有执行进程池的close或terminate方法，会报错）
