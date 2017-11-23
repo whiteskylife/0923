17353ed0d3f76626108c1a780bf02697b0596327
w-PC\w <981577040@qq.com>
2017-11-23 18:20:35 +0800

    commit

--- day9 线程 进程 协程/test.py
+++ day9 线程 进程 协程/test.py
@@ -24,16 +24,68 @@ import time
 #
 
 
-def xrange():
-    print('start')
-    start = 0
-    while True:
-        yield
-        start += 1
+# def xrange():
+#     print('start')
+#     start = 0
+#     while True:
+#         yield
+#         start += 1
+#
+# obj = xrange()
+# r = obj.__next__()
+# r = obj.__next__()
+# r = obj.__next__()
+# print(r)
+#
 
-obj = xrange()
-r = obj.__next__()
-r = obj.__next__()
-r = obj.__next__()
-print(r)
 
+# def worker_state(xxx, val):
+#     xxx.append(val)
+#     try:
+#         yield
+#     finally:
+#         xxx.remove(val)
+#
+# q = queue.Queue()
+# li = []
+# # li.append(1)
+# # q.get()
+# # li.remove(1)
+#
+# with worker_state(li, 1):           # 作用相当于上面注释的三行代码
+#     #print('before', li)
+#     q.get()
+
+
+
+
+
+
+
+# 上下文管理
+
+
+import contextlib
+import queue
+
+
+@contextlib.contextmanager
+def worker_state(xxx, val):
+    xxx.append(val)
+    try:
+        yield 123                  # 相当于断点
+    finally:
+        xxx.remove(val)
+
+q = queue.Queue()
+li = []
+# li.append(1)
+# q.get()
+# li.remove(1)
+
+with worker_state(li, 1) as f:           # 作用：管理上下文，功能相当于上面注释的三行代码
+    #print('before', li)
+    print(f)
+    q.get()
+
+#print('after', li)
