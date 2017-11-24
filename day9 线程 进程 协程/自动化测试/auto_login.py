868745efcc6b5021e8bc53e47c5d72025a4331d2
w-PC\w <981577040@qq.com>
2017-11-24 16:01:40 +0800

    commit

--- day9 线程 进程 协程/自动化测试/auto_login.py
+++ day9 线程 进程 协程/自动化测试/auto_login.py
@@ -1,12 +1,13 @@
-#!/usr/bin/env python
-# -*- coding utf-8 -*-
-# -*- coding:cp936 -*-
+#-*- coding:utf-8 -*-
+
+
 __author__ = 'Administrator'
-# 导入webdriver和time 类库
-from selenium import webdriver
+from selenium import webdriver      #  导入webdriver和time 类库
 import time
 
-print("------软件测试自动化：登录界面测试用例------")
+#from
+
+print "------软件测试自动化：登录界面测试用例------"
 # 创建testClass 类
 
 
@@ -16,19 +17,22 @@ class testClass(object):
             """ 打开浏览器 """
             global browser  # 申明browser为全局变量
             browser = webdriver.Chrome()   # 实例化Firefox类
-            time.sleep(2)   # 使程序暂停2秒，下同
+            time.sleep(3)   # 使程序暂停2秒，下同
             return "\n 已打开"
+
+        def test(self, url):
+            browser.get(url)    # get()方法打开url
 
         def keyinfo(self, url, unname, un, pwname, pw, authenticate_code, au):
             """定位用户名和密码输入框并写入数据"""
             browser.get(url)    # get()方法打开url
-            time.sleep(2)
-            browser.find_element_by_name(unname).send_keys(un)  # 定位username输入框并使用send_keys()向其写入数据
-            time.sleep(2)
-            browser.find_element_by_name(pwname).send_keys(pw)  # 定位password输入框并使用send_keys()向其写入数据
-            time.sleep(2)
-            browser.find_element_by_name(authenticate_code).send_keys(au)
-
+            time.sleep(0.1)             # 如果页面没有加载完成，继续向下执行可能会报错，要sleep几秒
+            browser.find_element_by_id(unname).send_keys(un)  # 定位username输入框并使用send_keys()向其写入数据
+            time.sleep(0.1)
+            browser.find_element_by_id(pwname).send_keys(pw)  # 定位password输入框并使用send_keys()向其写入数据
+            time.sleep(0.1)
+            browser.find_element_by_id(authenticate_code).send_keys(au)
+            browser.find_element_by_id('btn_login').click()
             return {"test url": url, "Username": un, "Password": pw}
 
         def keyyzm(self, yzmname, yzmpicid):
@@ -51,7 +55,7 @@ class testClass(object):
                 assert title in browser.title  # 使用assert断言页面title含某字符
             except AssertionError:  # 若页面title不含某字符（即登录失败），则引发AssertionError
                 print("%>_<% ...登陆失败") # AssertionError 的处理方式
-                screenshot = input("需要截图以保留fail现象吗?(Y/N)(图片以LoginErro.jpg保留在C:\): ")  # 询问是否需截图
+                screenshot = raw_input("需要截图以保留fail现象吗?(Y/N)(图片以LoginErro.jpg保留在C:\): ")  # 询问是否需截图
                 if screenshot == "Y":
                     browser.save_screenshot("C:\LoginErro.jpg")     # 使用save_screenshot方法截图并以LoginErro保存
             else:   # 若页面title含某字符（即登录成功），则执行else语句
@@ -60,23 +64,23 @@ class testClass(object):
 
         def closeB(self):
             """关闭浏览器"""
-            CloseB = input("关闭?(Y/N): ")  # 询问是否需要关闭
+            CloseB = raw_input("关闭?(Y/N): ")  # 询问是否需要关闭
             if CloseB == "Y":
                 browser.quit()      # 使用quit()关闭
             return "\n ------本次测试用例运行结束，感谢使用，再见O(∩_∩)O~.------"
 
-time.sleep(5)
+time.sleep(3)
 
 # 实例化testClass为 SucLogin：输入正确的用户名，密码，验证码登录
-
 SucLogin = testClass()
-print("1.打开浏览器", SucLogin.openB())
-print("2.打开测试url，并输入用户名，密码\n",)
+print "1.打开浏览器", SucLogin.openB()
+print "2.打开测试url，并输入用户名，密码"
+#SucLogin.keyinfo("https://tnew.gxyclub.com/toLogin")
 SucLogin.keyinfo("https://tnew.gxyclub.com/toLogin", "userName", "ltest1", "password", "123456", 'authCode', '0000')
 
 # print "3.请配合输入验证码\n", SucLogin.keyyzm("Yzm", "ValidCode")
 # print "4.验证登陆是否成功\n", SucLogin.verifylogin("Submit", U"首页")
-print("5.关闭浏览器\n", SucLogin.closeB())
+#print("5.关闭浏览器\n", SucLogin.closeB())
 
 # 根据需要实例化testClass为XXXX，修改相关数据
 
