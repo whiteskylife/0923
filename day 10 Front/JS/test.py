98a53a3f326a8169e1df76c400589edb80842083
w-PC\w <981577040@qq.com>
2018-01-02 21:57:07 +0800

    commit

--- day 10 Front/JS/test.py
+++ day 10 Front/JS/test.py
@@ -1,13 +1,32 @@
 #!/usr/bin/env python
 # -*- coding utf-8 -*-
 
-class Foo:
-    def __init__(self, name):
-        self.name = name
-    def sayName(self):
-        print(self.name)
+'''
+li = []
+
+for i in range(10):
+    def f1():
+        return i
+    li.append(f1)
+    # 相当于 li.append(lambda: i)
+print(i)
+
+print(li)
+print(li[0]())
+print(li[1]())
 
 
-obj1 = Foo('we')
-obj1.sayName()
-obj2 = Foo('wee')
+
+def xo():
+    return x
+
+相当于： lambda:x
+'''
+
+
+li = [lambda:x for x in range(9)]
+print(li)
+print(li[1]())      # 此处才开始调用列表中的函数，此时x的值已经循环到8了
+
+
+# 小结：函数只有在被调用时才会执行
