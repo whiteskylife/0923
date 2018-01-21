f07c09ed43a6c36df433e8613cf8f298574d3ee5
Administrator <981577040@qq.com>
2018-01-21 15:24:12 +0800

    commit

--- day 11 web frame/whisky_frame/controller.py
+++ day 11 web frame/whisky_frame/controller.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python
 # -*-coding:utf-8 -*-
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
 import os
-
+from jinja2 import Template
 
 def new():
-    html_file = open(os.path.join('views', "html basic.html"), 'r')
-    data = html_file.read()
-    html_file.close()
-    return data
-    # return '<html><head><body><h1 style="color:red;">asdf</h1></body></head></html>'
+    # html_file = open(os.path.join('views', "html basic.html"), 'r')
+    # data = html_file.read()
+    # html_file.close()
+    # data = data.replace("{{item}}", str(time.time()))
+    # return data
 
+    f = open(os.path.join('views', 'html basic.html'))
+    result = f.read()
+
+    template = Template(result)
+    data = template.render(name='John Doe', user_list=['alex', 'eric','xiaojun', 'sb', 'alex', '222'])
+    return data.encode('utf-8')
 
 def index():
     return 'index'
