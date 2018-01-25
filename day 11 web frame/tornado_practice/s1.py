5e8bc02b0b228a000aff694d3fcdabd6c7bda8d6
Administrator <981577040@qq.com>
2018-01-25 08:27:09 +0800

    commit

--- day 11 web frame/tornado_practice/s1.py
+++ day 11 web frame/tornado_practice/s1.py
@@ -10,19 +10,23 @@ INPUTS_LIST = []
 
 
 # 处理请求的类:
-class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        # self.write("Hello, world")
-        name = self.get_argument('xxx', None)     # 获取用户提交的数据
-        if name:
-            INPUTS_LIST.append(name)
-        self.render('s1.html', npm="NPM_test", xxxooo=INPUTS_LIST)
+# class MainHandler(tornado.web.RequestHandler):
+#     def get(self):
+#         # self.write("Hello, world")
+#         name = self.get_argument('xxx', None)     # 获取用户提交的数据
+#         if name:
+#             INPUTS_LIST.append(name)
+#         self.render('s1.html', npm="NPM_test", xxxooo=INPUTS_LIST)
 
     # def post(self, *args, **kwargs):
     #     name = self.get_argument('xxx')     # 获取用户提交的数据
     #     INPUTS_LIST.append(name)
     #     self.render("s1.html", xxxooo=INPUTS_LIST)
 
+
+class IndexHandler(tornado.web.RequestHandler):
+    def get(self, *args, **kwargs):
+        self.render('chouti.html')
 
 settings = {
     "template_path": "tpl",        # 模板路径配置(存放HTML)
@@ -34,9 +38,10 @@ settings = {
 
 # 路由映射，路由系统
 application = tornado.web.Application([
-    (r"/index", MainHandler),
+    # (r"/index", MainHandler),
+    (r"/index", IndexHandler),
 ], **settings)
 
 if __name__ == "__main__":
-    application.listen(8888)
+    application.listen(80)
     tornado.ioloop.IOLoop.instance().start()
