c9e12a1454ce17d2fb6f2f37a679c0c5853fdf06
w-PC\w <981577040@qq.com>
2018-02-06 18:13:51 +0800

    commit

--- day 11 web frame/tornado_cookie2/app.py
+++ day 11 web frame/tornado_cookie2/app.py
@@ -7,10 +7,11 @@ import tornado.web
 
 class IndexHandler(tornado.web.RequestHandler):
     def get(self):
-        if self.get_argument('u', None) in ['whisky', 'alex']:
+        if self.get_argument('u', None) in ['whisky', 'alex']:     # 设置u值默认为None，否则访问不带u参数会报400错误
             self.set_cookie('name', self.get_argument('u'))
+            self.write('登录成功')
         else:
-            self.write('请登录')
+            self.write('请登录')       # 省去写前端页面，直接在前端页面输出write方法中的内容
 
 
 class ManagerHandler(tornado.web.RequestHandler):
@@ -29,8 +30,13 @@ settings = {
 application = tornado.web.Application([
     (r"/index", IndexHandler),
     (r"/manager", ManagerHandler),
-])
+], **settings)
 
 if __name__ == "__main__":
-    application.listen(8888)
+    application.listen(8000)
     tornado.ioloop.IOLoop.instance().start()
+
+
+# 登录测试：http://127.0.0.1:8000/index?u=whisky
+
+# 基于cookie实现用户验证
