6c58394d1716faaeb2ee4bcc881fa33296710a68
w-PC\w <981577040@qq.com>
2018-02-26 14:37:21 +0800

    save method

--- day 11 web frame/Form_Validation/app.py
+++ day 11 web frame/Form_Validation/app.py
@@ -6,6 +6,8 @@ import tornado.web
 import re
 # form验证：要求前端name字段和后端类中封装的字段名字一致
 # checkbox验证
+# 文件名验证匹配不支持中文汉字
+
 
 class IPFiled:
     REGULAR = "^(25[0-5]|2[0-4]\d|[0-1]?\d?\d)(\.(25[0-5]|2[0-4]\d|[0-1]?\d?\d)){3}$"   # 静态 字段
@@ -39,13 +41,13 @@ class IPFiled:
         else:       # 当81行要求输入不为空时候 翻译：否则，如果 有 内容，即不为空时
             # 用户有输入要求不允许为空,这一要验证正则了。
             if not input_value.strip():  # 输入为空,返回错误信息。 错误信息字典
-                if self.error_dict.get('required',None):  # 81行定义的错误信息。如果未定义则自己定义
+                if self.error_dict.get('required', None):  # 81行定义的错误信息。如果未定义则自己定义
                     self.error = self.error_dict['required']
                 else:
                     self.error = "%s is required" % name     # 默认错误信息
-            else:  # 用户输入不为空,
-                ret = re.match(IPFiled.REGULAR, input_value) #获取 静态字段的正则字符串,跟用户的input输入内容匹配
-                if ret: # 匹配通过,
+            else:           # 用户输入不为空,
+                ret = re.match(IPFiled.REGULAR, input_value)  # 获取 静态字段的正则字符串,跟用户的input输入内容匹配
+                if ret:     # 匹配通过,
                     self.is_valid = True
                     self.value = input_value    # self.value = ret.group()也是一样的
                 else:  # 匹配不通过,返回错误字典
@@ -145,7 +147,7 @@ class CheckBoxFiled:
 
 
 class FileFiled:
-    REGULAR = "^(\w+\.pdf)|(\w+\.mp3)|(\w+\.py)$"  # 静态 字段
+    REGULAR = "^(\w+\.pdf)|(\w+\.mp3)|(\w+\.py)|(\w+\.txt)$"  # 静态 字段
 
     def __init__(self, error_dict=None, required=True):
         """
@@ -171,8 +173,7 @@ class FileFiled:
         :param all_file_name_list: 所有文件的文件名
         :return:
         """
-
-        self.name = name
+        self.name = name            # 上传文件的字段名fafafa
         if not self.required:
             # 用户输入允许为空
             self.is_valid = True      # 设置封装值为真，表示验证通过
@@ -187,22 +188,46 @@ class FileFiled:
             else:
                 # 循环所有文件名
                 for file_name in all_file_name_list:
-                    ret = re.match(FileFiled.REGULAR, file_name) # 获取 静态字段的正则字符串,跟用户的input输入内容匹配
+                    ret = re.match(FileFiled.REGULAR, file_name)        # 文件名中含有中文无法匹配
                     if not ret:
                         self.is_valid = False       # 验证没有通过
-                        if self.error_dict.get('required', None):
-                            self.error = self.error_dict['required']
+                        if self.error_dict.get('valid', None):
+                            self.error = self.error_dict['valid']
                         else:
-                            self.error = "%s is required" % name
+                            self.error = "%s is invalid" % name
                         break
-                    else:  # 有一个文件正确就加入文件列表
+                    else:  # 可能会上传多个文件，有一个文件正确就加入文件列表
                         self.value.append(file_name)
+
+    def save(self, request, path='statics'):
+        """
+        :param request: request中封装了所有用户发过来的前端中的数据
+        :param path:
+        :return:
+        """
+        file_metas = request.files.get(self.name)      # 所有文件列表,self.name取前端的name变量值,get得到文件列表[{文件1}, {文件2}]
+        # print(self.name)        # 输出fafafa
+        # print(file_metas)
+        for meta in file_metas:                     # 循环文件列表，列表中包含很多字典，每个字典包含一个文件，meta为单个字典
+            print('meta in for loop---', meta)
+            file_name = meta['filename']
+            import os
+            new_file_name = os.path.join(path, file_name)
+            if file_name and file_name in self.value:       # 文件名为空不上传，且文件名要在成功的列表中才会上传保存
+                with open(new_file_name, 'wb') as up:
+                    up.write(meta['body'])                  # 字典中的body字段包含文件内容
+        # print(file_metas)输出
+        # [{'body': b'1', 'content_type': 'text/plain', 'filename': '21.txt'},
+        # {'body': b'1', 'content_type': 'text/plain', 'filename': '12.txt'}]
+        # print('meta in for loop---', meta)输出
+        # meta in for loop - -- {'body': b'1', 'content_type': 'text/plain', 'filename': '21.txt'}
+        # meta in for loop - -- {'body': b'1', 'content_type': 'text/plain', 'filename': '12.txt'}
 
 
 class BaseForm:
     def check_valid(self, handle):
         """
-         获取用户提交的数据，将用户提交的数据传递到对应的类中进行正则表达式匹配，返回匹配结果和正确/错误等自定义信息
+         获取用户提交的数据，将用户提交的数据传递到对应的类中进行正则表达式匹配，再调用类中匹配结果和正确/错误等自定义信息
         :param handle: post方法中的self，里面封装了get_argument方法，可以直接取前端传过来的参数值
         :return:返回正则匹配结果：成功-True；失败：false，正则匹配成功后返回一个字典，失败返回错误信息
         """
@@ -211,29 +236,28 @@ class BaseForm:
         success_value_dict = {}
         for key, regular in self.__dict__.items():    # 遍历self中封装的参数
             # key是host、ip、port、phone
-            # regular:当调用HomeForm类时值为IPField(required=True)
-            if type(regular) == CheckBoxFiled:
+            # regular:当调用HomeForm类时值为IPField(required=True)对象
+            if type(regular) == CheckBoxFiled:      # 复选框功能
                 # print('question：--------' + str(type(regular)))
                 # print('question：--------' + str(CheckBoxFiled))
                 input_value = handle.get_arguments(key)  # 对于CheckBox来说，input_value就是用户提交name="favor"的列表
-            elif type(regular) == FileFiled:
-                # 文件上传功能，获取文件名,放入一个列表中
-                file_list = handle.request.files.get(key)   # [{'body':'xx', 'filename':'xx'},{'body':'xx', 'filename':'xx'}]
+            elif type(regular) == FileFiled:             # 文件上传功能，获取文件名,放入一个列表中
+                file_list = handle.request.files.get(key)  # [{'body':'xx', 'filename':'xx'},{'body':'xx', 'filename':'xx'}]
+                # print(file_list)
                 input_value = []
-                # if file_list:
-                for item in file_list:
-                    input_value.append(item['filename'])
-                # 所有文件名验证
-            else:
+                if file_list:       # 如果没有提交文件，会报错，此处用个if避免报错
+                    for item in file_list:
+                        input_value.append(item['filename'])    # 加入列表，由validate方法进行所有文件名验证
+            else:                                       # 输入内容功能
                 input_value = handle.get_argument(key)
-            # 将具体的验证放在IPFiled中
-            regular.validate(key, input_value)
+                # print(input_value + '输入内容功能==========')
+            # 将具体的验证放在validate方法中
+            regular.validate(key, input_value)  # regular为每次循环时HomeForm类中封装的对象
             if regular.is_valid:        # 如果正则匹配成功（调用IPField中封装的字段）
                 success_value_dict[key] = regular.value         # 把匹配到的值放到字典中
             else:
                 error_message_dict[key] = regular.error         # 把错误信息放到字典中
-                flag = False
-
+                flag = False                                    # HomeForm类中，有一种对象不符合正则匹配，就返回失败
         return flag, success_value_dict, error_message_dict
 
 
@@ -273,8 +297,9 @@ class HomeHandler(tornado.web.RequestHandler):
 
         obj = HomeForm()
         is_valid, success_dict, error_dict = obj.check_valid(self)
-        if is_valid:
+        if is_valid:            # 如果HomeForm类中所有对象校验成功
             print('success', success_dict)
+            obj.fafafa.save(self.request)   # self.request中封装了所有用户发过来的前端中的数据
         else:
             print('error', error_dict)
             self.render('home.html', error_dict=error_dict)     # 错误信息传递给前端渲染
